4202a3e43ebfb45269b81fb2d976c0de8c20840a
Sasha <aleksikunya@mail.ru>
2020-11-30 22:14:29 +0300

    3 and 4 tasks

---
+++ js/script2.js
@@ -0,0 +1,8 @@
+// 3 задание
+let width = 10, 
+    height = 23, 
+    SPryam;
+
+SPryam = width * height;
+
+console.log(SPryam);
---
+++ js/script3.js
@@ -0,0 +1,8 @@
+// 4 задание
+let h = 10,
+    r = a7 / 2,
+    VCilindra;
+
+VCilindra =  Math.PI * Math.pow(r, 2) * h;
+
+console.log(VCilindra);
